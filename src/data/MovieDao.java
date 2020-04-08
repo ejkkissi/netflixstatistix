75da29e3e7ab4463d7c1864d6146a975ff904c3a
Emmanuel <57803600+ejkkissi@users.noreply.github.com>
2020-04-08 15:45:35 +0200

    wrote MovieDao

--- src/data/MovieDao.java
+++ src/data/MovieDao.java
@@ -1,23 +1,84 @@
 package data;
 
+import managers.DBManager;
+import model.Episode;
 import model.Movie;
 import model.Profile;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieDao implements IMovieDao {
     @Override
     public List<Movie> getAllMovies() {
-        return null;
+        List<Movie> list = new ArrayList<>();
+        String sql = "SELECT * FROM movie";
+        DBManager.getInstance().query(sql, rs -> {
+            try {
+                while (rs.next()) {
+                    Movie m = new Movie(
+                            rs.getInt("id"),
+                            rs.getString("title"),
+                            rs.getInt("duration"),
+                            rs.getString("genre"),
+                            rs.getString("language"),
+                            rs.getInt("age_indication")
+                    );
+                    list.add(m);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        return list;
     }
 
     @Override
     public Movie getMovieById(int id) {
-        return null;
+        Movie m = new Movie();
+        String sql = "SELECT * FROM movie WHERE id = " + id;
+        DBManager.getInstance().query(sql, rs -> {
+            try {
+                while (rs.next()) {
+                    m.setId(rs.getInt("id"));
+                    m.setTitle(rs.getString("title"));
+                    m.setDuration(rs.getInt("duration"));
+                    m.setGenre(rs.getString("genre"));
+                    m.setLanguage(rs.getString("language"));
+                    m.setAgeIndication(rs.getInt("age_indication"));
+                }
+            } catch (SQLException sqle) {
+                sqle.printStackTrace();
+            }
+        });
+        return m;
     }
 
     @Override
     public List<Movie> getMoviesWatchedBy(Profile profile) {
-        return null;
+        List<Movie> list = new ArrayList<>();
+        String sql = "SELECT * FROM movie\n"
+                    +"JOIN profile_movie ON profile_movie.movie = movie.id\n"
+                    +"WHERE profile_movie.profile_email = " + profile.getAccountEmail()
+                    +"AND profile_movie.profile_name = " + profile.getName();
+        DBManager.getInstance().query(sql, rs -> {
+            try {
+                while (rs.next()) {
+                    Movie m = new Movie(
+                            rs.getInt("id"),
+                            rs.getString("title"),
+                            rs.getInt("duration"),
+                            rs.getString("genre"),
+                            rs.getString("language"),
+                            rs.getInt("age_indication")
+                    );
+                    list.add(m);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        return list;
     }
 }
--- src/model/Movie.java
+++ src/model/Movie.java
@@ -5,10 +5,38 @@ public class Movie extends Program {
     private String language;
     private int ageIndication;
 
+    public Movie() {
+
+    }
+
     public Movie(int id, String title, int duration, String genre, String language, int ageIndication) {
         super(id, title, duration);
         this.genre = genre;
         this.language = language;
+        this.ageIndication = ageIndication;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getAgeIndication() {
+        return ageIndication;
+    }
+
+    public void setAgeIndication(int ageIndication) {
         this.ageIndication = ageIndication;
     }
 }
