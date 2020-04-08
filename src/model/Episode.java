21605b460fb0b52b4c3565bb8fd19240b02ea5ae
Emmanuel <57803600+ejkkissi@users.noreply.github.com>
2020-04-08 13:25:43 +0200

    wrote episodedao

--- src/model/Episode.java
+++ src/model/Episode.java
@@ -2,11 +2,31 @@ package model;
 
 public class Episode extends Program {
     private int episodeNumber;
-    private int season;
+    private String season;
 
-    public Episode(int id, String title, int duration, int episodeNumber, int season) {
+    public Episode() {
+
+    }
+
+    public Episode(int id, String title, int duration, int episodeNumber, String season) {
         super(id, title, duration);
         this.episodeNumber = episodeNumber;
+        this.season = season;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
         this.season = season;
     }
 }
--- src/model/Program.java
+++ src/model/Program.java
@@ -5,9 +5,37 @@ public abstract class Program {
     private String title;
     private int duration;
 
+    public Program() {
+
+    }
+
     public Program(int id, String title, int duration) {
         this.id = id;
         this.title = title;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
         this.duration = duration;
     }
 }
